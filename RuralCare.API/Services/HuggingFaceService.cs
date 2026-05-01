911e13e4adf9ecaa6388125c9fd276f11966bee9
vermf <vermf@VIJAYA999_80021>
2026-05-01 17:49:40 -0500

    created and deleted the HuggingFace APiKey for security reasons, GitHub refuses to commit and push this code with the hard-coded secuiity key / ApiCode.

--- RuralCare.API/Services/HuggingFaceService.cs
+++ RuralCare.API/Services/HuggingFaceService.cs
@@ -21,8 +21,8 @@
             {
                 /* Commenting as a security feature, GitHub does not push code to public repository with a secret code in the files. */
                 /*
-                var apiKey = _configuration["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"];  // Hugging Face API Key. 
-                var modelName = _configuration["google/gemma-4-E2B-it"];  // Hugging Face Gemma 4 model name.
+                var apiKey = _configuration["ApiKey"];  // Hugging Face API Key. 
+                var modelName = _configuration["ModelName"];  // Hugging Face Gemma 4 model name.
                 */
                 var apiKey = "UN_COMMENT_ABOVE_CODE";
                 var modelName = "UN_COMMENT_MODEL_NAME_ABOVE"; 
